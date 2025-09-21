88a8b1c1bd1ebcc8f2040bef4eb11d1e4f3995cf
Thanhdat1293 <phongvanvoky1293@gmail.com>
2025-09-21 13:34:54 +0700

    Bai7.java

--- Bai7.java
+++ Bai7.java
@@ -3,15 +3,12 @@ import java.util.Scanner;
 public class Bai7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Nhập số lượng phần tử của mảng
         System.out.print("Nhập số phần tử của mảng: ");
         int n = scanner.nextInt();
 
         int[] arr = new int[n];
         int sum = 0;
 
-        // Nhập các phần tử vào mảng
         System.out.println("Nhập các phần tử của mảng:");
         for (int i = 0; i < n; i++) {
             System.out.print("Phần tử thứ " + (i + 1) + ": ");
@@ -19,7 +16,6 @@ public class Bai7 {
             sum += arr[i]; // cộng dồn
         }
 
-        // In ra kết quả
         System.out.println("Tổng các phần tử trong mảng là: " + sum);
 
         scanner.close();
