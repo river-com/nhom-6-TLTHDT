0ff98bf635892d902af7eb3ccd48ffcfd13d906d
Thanhdat1293 <phongvanvoky1293@gmail.com>
2025-09-21 13:38:00 +0700

    Add files via upload

---
+++ Bai8.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Bai8 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 1, 4, 2, 5, 1, 1}; // Mảng mẫu
+
+        // Dùng HashMap để đếm số lần xuất hiện
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        // Tìm phần tử có số lần xuất hiện nhiều nhất
+        int maxCount = 0;
+        int mostFrequent = arr[0];
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > maxCount) {
+                maxCount = entry.getValue();
+                mostFrequent = entry.getKey();
+            }
+        }
+
+        System.out.println("Phần tử xuất hiện nhiều nhất: " + mostFrequent);
+        System.out.println("Số lần xuất hiện: " + maxCount);
+    }
+}
